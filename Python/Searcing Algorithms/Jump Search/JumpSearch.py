a9ad15042e7155da560050484c6208e6bf2ccc44
zumrudu-anka <osmandurdag@hotmail.com>
2019-08-13 02:57:26 +0300

    added jump search

---
+++ Python/Searcing Algorithms/Jump Search/JumpSearch.py
@@ -0,0 +1,17 @@
+import math
+def JumpSearch(liste, jump_length, value):
+    for i in range(jump_length, len(liste), jump_length):
+        if liste[i] > value:
+            for j in liste[(i - jump_length): (i + 1)]:
+                if j == value:
+                    return True, liste.index(j)
+        else:
+            if (i + jump_length) >= len(liste):
+                for j in liste[i:len(liste)]:
+                    if j == value:
+                        return True, liste.index(j)
+    return False, -1
+
+liste = [1, 3, 7, 9, 11, 13, 19, 29, 38, 46, 51, 87, 99, 110, 132, 159]
+found, index = JumpSearch(liste, int(math.sqrt(len(liste))), 159)
+print(found, ',', index)
