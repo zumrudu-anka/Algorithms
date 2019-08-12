e54b4cb3444acc8710abd9b677d7143dd78908b7
zumrudu-anka <osmandurdag@hotmail.com>
2019-08-13 02:59:33 +0300

    changed jumpsearch.py

--- Python/Searcing Algorithms/Jump Search/JumpSearch.py
+++ Python/Searcing Algorithms/Jump Search/JumpSearch.py
@@ -1,5 +1,6 @@
 import math
-def JumpSearch(liste, jump_length, value):
+def JumpSearch(liste, value):
+    jump_length = int(math.sqrt(len(liste)))
     for i in range(jump_length, len(liste), jump_length):
         if liste[i] > value:
             for j in liste[(i - jump_length): (i + 1)]:
@@ -13,5 +14,5 @@ def JumpSearch(liste, jump_length, value):
     return False, -1
 
 liste = [1, 3, 7, 9, 11, 13, 19, 29, 38, 46, 51, 87, 99, 110, 132, 159]
-found, index = JumpSearch(liste, int(math.sqrt(len(liste))), 159)
+found, index = JumpSearch(liste, 159)
 print(found, ',', index)
