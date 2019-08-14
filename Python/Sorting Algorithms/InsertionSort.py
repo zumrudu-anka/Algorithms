5d99f2c03559e09293a951ea49e15d32cf0a13d2
zumrudu-anka <osmandurdag@hotmail.com>
2019-08-14 20:10:42 +0300

    added insertionsort.py

---
+++ Python/Sorting Algorithms/InsertionSort.py
@@ -0,0 +1,12 @@
+def InsertionSort(list):
+    for i in range(1,len(list)):
+        for j in range(i - 1, 0, -1):
+            if list[j] < list[j - 1]:
+                temp = list[j]
+                list[j] = list[j - 1]
+                list[j - 1] = temp
+    return list
+
+list = [16, 13, 55, 27, 35, 18, 1, 0, 14, 13, 14, 19, 138]
+
+print(InsertionSort(list))
